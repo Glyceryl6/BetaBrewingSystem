f3f03b39577fed49981dc608cfb676e5d8ca184d
glyceryl <494192175@qq.com>
2022-08-14 18:19:50 +0800

    给药水瓶添加物品模型

--- src/main/java/com/glyceryl6/cauldron/Cauldron.java
+++ src/main/java/com/glyceryl6/cauldron/Cauldron.java
@@ -1,11 +1,14 @@
 package com.glyceryl6.cauldron;
 
 import com.glyceryl6.cauldron.block.BlockBrewingCauldron;
+import com.glyceryl6.cauldron.block.EntityBrewingCauldron;
+import com.glyceryl6.cauldron.item.ItemBrewingCauldronPotion;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlockSpecial;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.MinecraftForge;
@@ -15,6 +18,7 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -30,6 +34,8 @@ public class Cauldron {
     public static Item BREWING_CAULDRON;
     @ObjectHolder("beta_brewing_system:brewing_cauldron_block")
     public static BlockBrewingCauldron BLOCK_BREWING_CAULDRON;
+    @ObjectHolder("beta_brewing_system:potion_cauldron")
+    public static ItemBrewingCauldronPotion POTION_ITEM;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
@@ -38,7 +44,8 @@ public class Cauldron {
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-
+        ResourceLocation resource = new ResourceLocation(MODID, "brewing_cauldron_block_entity");
+        GameRegistry.registerTileEntity(EntityBrewingCauldron.class, resource);
     }
 
     @SubscribeEvent
@@ -52,17 +59,20 @@ public class Cauldron {
 
     @SubscribeEvent
     public void registerItem(RegistryEvent.Register<Item> event) {
+        POTION_ITEM = (ItemBrewingCauldronPotion)(new ItemBrewingCauldronPotion())
+                .setUnlocalizedName("potion_cauldron").setRegistryName(MODID, "potion_cauldron");
         BREWING_CAULDRON = new ItemBlockSpecial(BLOCK_BREWING_CAULDRON);
         BREWING_CAULDRON.setCreativeTab(CreativeTabs.BREWING);
         BREWING_CAULDRON.setUnlocalizedName("brewing_cauldron");
         BREWING_CAULDRON.setRegistryName(MODID, "brewing_cauldron");
-        event.getRegistry().register(BREWING_CAULDRON);
+        event.getRegistry().registerAll(BREWING_CAULDRON, POTION_ITEM);
     }
 
     @SubscribeEvent
     @SideOnly(Side.CLIENT)
     public void registerModel(ModelRegistryEvent event) {
         ModelLoader.setCustomModelResourceLocation(BREWING_CAULDRON, 0, new ModelResourceLocation(BREWING_CAULDRON.getRegistryName(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(POTION_ITEM, 0, new ModelResourceLocation(POTION_ITEM.getRegistryName(), "inventory"));
     }
 
 }
