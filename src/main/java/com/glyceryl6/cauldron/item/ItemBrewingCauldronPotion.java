5877b3fcd273e3cd14cb1e4b2b15c6c97ee9b26b
glyceryl <494192175@qq.com>
2022-08-16 09:12:17 +0800

    修复药水显示的时长与玩家效果时长同步的bug

--- src/main/java/com/glyceryl6/cauldron/item/ItemBrewingCauldronPotion.java
+++ src/main/java/com/glyceryl6/cauldron/item/ItemBrewingCauldronPotion.java
@@ -4,16 +4,19 @@ import com.glyceryl6.cauldron.Cauldron;
 import com.glyceryl6.cauldron.block.PotionCauldron;
 import com.glyceryl6.cauldron.block.PotionHelperCauldron;
 import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
-import net.minecraft.item.ItemPotion;
+import net.minecraft.item.EnumAction;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
-import net.minecraft.util.NonNullList;
-import net.minecraft.util.text.Style;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.translation.I18n;
@@ -25,13 +28,12 @@ import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.HashMap;
 import java.util.List;
 
-public class ItemBrewingCauldronPotion extends ItemPotion {
+public class ItemBrewingCauldronPotion extends Item {
 
     private final HashMap<Object, Object> a = new HashMap<>();
 
     public ItemBrewingCauldronPotion() {
         setMaxStackSize(1);
-        setMaxDamage(0);
         setHasSubtypes(false);
     }
 
@@ -43,6 +45,14 @@ public class ItemBrewingCauldronPotion extends ItemPotion {
             this.a.put(i, list);
         }
         return list;
+    }
+
+    public static NBTBase getPotionColorTag(ItemStack itemStack, String key) {
+        if (itemStack.hasTagCompound() && itemStack.getTagCompound() != null) {
+            NBTTagCompound compoundTag = itemStack.getTagCompound().getCompoundTag("Color");
+            return compoundTag.getTag(key);
+        }
+        return null;
     }
 
     @ParametersAreNonnullByDefault
@@ -61,10 +71,26 @@ public class ItemBrewingCauldronPotion extends ItemPotion {
             return new ItemStack(Items.GLASS_BOTTLE);
         }
         if (entityLiving instanceof EntityPlayer) {
-            itemStack.stackSize--;
+            if (!((EntityPlayer) entityLiving).capabilities.isCreativeMode) {
+                itemStack.stackSize--;
+            }
             ((EntityPlayer) entityLiving).inventory.addItemStackToInventory(new ItemStack(Items.GLASS_BOTTLE));
         }
         return itemStack;
+    }
+
+    public int getMaxItemUseDuration(ItemStack itemStack) {
+        return 32;
+    }
+
+    public EnumAction getItemUseAction(ItemStack itemStack) {
+        return EnumAction.DRINK;
+    }
+
+    @ParametersAreNonnullByDefault
+    public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
+        player.setActiveHand(hand);
+        return new ActionResult(EnumActionResult.SUCCESS, player.getHeldItem(hand));
     }
 
     public String getItemStackDisplayName(ItemStack itemStack) {
@@ -75,37 +101,34 @@ public class ItemBrewingCauldronPotion extends ItemPotion {
         return new TextComponentTranslation(string).getFormattedText();
     }
 
+    @SideOnly(Side.CLIENT)
     @SuppressWarnings("deprecation")
     public void addInformation(ItemStack itemStack, World world, List<String> list, ITooltipFlag bool) {
         if (itemStack.getItemDamage() == 0) return;
         List<PotionEffect> list1 = Cauldron.POTION_ITEM.a_(itemStack);
         if (list1 != null && !list1.isEmpty()) {
             for (PotionEffect effect : list1) {
-                String string = new TextComponentTranslation(effect.getEffectName()).getFormattedText();
+                String string = I18n.translateToLocal(effect.getEffectName()).trim();
                 if (effect.getAmplifier() > 0) {
                     string = string + " " + I18n.translateToLocal("potion.potency." + effect.getAmplifier()).trim();
                 }
                 if (effect.getDuration() > 20) {
-                    string = string + " (" + PotionCauldron.getDurationString(effect) + ")";
+                    string = string + " (" + Potion.getPotionDurationString(effect, 1.0F) + ")";
                 }
                 if (PotionCauldron.potionTypes[Potion.getIdFromPotion(effect.getPotion())].isUsable()) {
-                    list.add(new TextComponentTranslation(string).setStyle((new Style()).setColor(TextFormatting.RED)).getFormattedText());
+                    list.add(TextFormatting.RED + I18n.translateToLocal(string));
                     continue;
                 }
-                list.add(new TextComponentTranslation(string).setStyle((new Style()).setColor(TextFormatting.BLUE)).getFormattedText());
+                list.add(TextFormatting.BLUE + I18n.translateToLocal(string));
             }
         } else {
-            list.add(new TextComponentTranslation("effect.none").setStyle((new Style()).setColor(TextFormatting.GRAY)).getFormattedText());
+            list.add(TextFormatting.GRAY + I18n.translateToLocal("effect.none"));
         }
     }
 
-    @ParametersAreNonnullByDefault
-    public void getSubItems(CreativeTabs tabs, NonNullList<ItemStack> stacks) {}
-
     @SideOnly(Side.CLIENT)
-    @ParametersAreNonnullByDefault
     public boolean hasEffect(ItemStack itemStack) {
-        return true;
+        return itemStack.getItemDamage() != 0;
     }
 
 }
